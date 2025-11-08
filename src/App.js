70bf2707e8a60e8d63bff28e093eb81d1c103b0c
X20M29ABD <rketailservices@gmail.com>
2025-11-08 06:11:39 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <nav>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My App</h1>
+      </nav>
+      <main>
+        <p>This is a simple React application.</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
